62ad9e0bf8e3fb1295a34a601cf266e9707ef428
Luana Bringel <83998722+luanabringel@users.noreply.github.com>
2023-09-18 23:47:07 -0300

    Update Local.hs

--- Local/Local.hs
+++ Local/Local.hs
@@ -1,17 +1,18 @@
-module Local.Local (Local(Local), Locais(Locais),
-    getIdLocal,
+module Haskell.Local (Local(Local), Locais(Locais),
+    --getIdLocal,
     getCapacidadeLocal,
     getLocais,
     getLocaisFromList,
     getLocaisFromLista,
     getLocaisPuros,
-    getLocalPeloId,
+    --getLocalPeloId,
     localToString,
     getNomeLocal,
     getRecursosList,
     getRecursosLocal,
     getRecursosLocais,
-    getRecursosLocalToString) where
+    getRecursosLocalToString,
+    writeArquivoLocal) where
 import System.IO
 import System.Directory
 import Haskell.Util
@@ -22,9 +23,11 @@ import System.IO.Error
 import System.Process
 import Control.Monad (when)
 import Text.Printf
+import Data.List
+import Haskell.Agenda2
 
 data Local = Local {
-    idLocal :: Int,
+    --idLocal :: Int,
     nomeLocal :: String,
     recursosLocal :: [String],
     capacidadeLocal :: Int
@@ -35,8 +38,8 @@ newtype Locais = Locais {
 } deriving Show
 
 --------- Local Getters -----------
-getIdLocal :: Local -> Int
-getIdLocal Local {idLocal = i} = i
+--getIdLocal :: Local -> Int
+--getIdLocal Local {idLocal = i} = i
 
 getNomeLocal :: Local -> String
 getNomeLocal Local {nomeLocal = n} = n
@@ -46,15 +49,15 @@ getRecursosLocal Local {recursosLocal = r} = r
 
 getRecursosLocais :: [Local] -> String
 getRecursosLocais [] = []
-getRecursosLocais (l:ls) = show (getIdLocal l) ++ "," ++ getRecursosLocalToString (getRecursosLocal l) ++ getRecursosLocais ls
+getRecursosLocais (l:ls) = show (getNomeLocal l) ++ "," ++ getRecursosLocalToString (getRecursosLocal l) ++ getRecursosLocais ls
 
 getCapacidadeLocal :: Local -> Int
 getCapacidadeLocal Local {capacidadeLocal = c} = c
 
-getLocalPeloId :: Int -> [Local] -> Maybe Local
-getLocalPeloId id [] = Nothing
-getLocalPeloId id (l:ls) = if id == getIdLocal l then Just l
-                        else getLocalPeloId id ls
+--getLocalPeloId :: Int -> [Local] -> Maybe Local
+--getLocalPeloId id [] = Nothing
+--getLocalPeloId id (l:ls) = if id == getIdLocal l then Just l
+                        --else getLocalPeloId id ls
 
 getLocaisFromLista :: [(Int, Local)] -> [Local]
 getLocaisFromLista [] = []
@@ -68,28 +71,28 @@ getRecursosLocalToString [] = []
 getRecursosLocalToString (r:rs) = if not (null rs) then r ++ "," ++ getRecursosLocalToString rs else r ++ "\n"
 
 localToString :: Local -> String
-localToString Local {idLocal = i, nomeLocal = n, recursosLocal = r, capacidadeLocal = c} =  "Id: " ++ show i ++"\n" ++
-                                                                                            "Nome: " ++ n ++ "\n" ++
-                                                                                            "Recursos: " ++ getRecursosLocalToString r  ++
-                                                                                            "Capacidade: " ++ show c
+localToString Local {nomeLocal = n, recursosLocal = r, capacidadeLocal = c} = "Nome: " ++ n ++ "\n" ++
+                                                                            "Recursos: " ++ getRecursosLocalToString r  ++
+                                                                            "Capacidade: " ++ show c
 
 --------- Escrever Arquivo Local para Usuário ------------
-writeArquivoLocal :: [Local] -> IO()
+
+writeArquivoLocal :: Local -> IO()
 writeArquivoLocal local = do
-    arq <- openFile "../Arquivos/Locais.csv" AppendMode
-    arq1 <- openFile "../Arquivos/RecursosLocal.csv" AppendMode
+    arq <- openFile "Locais.csv" AppendMode
+    arq1 <- openFile "RecursosLocal.csv" AppendMode
 
     print local -- mudar isso, mas fazer depois
 
-    let dataRecursosLocal = getRecursosLocais local
+    writeAgendaLocal (getNomeLocal local)
+    let dataRecursosLocal = getRecursosLocalToString (getRecursosLocal local)
     hPutStr arq1 dataRecursosLocal
     hPutStr arq (toWrite local)
     hClose arq
     hClose arq1
 
-toWrite :: [Local] -> String
-toWrite [] = []
-toWrite (x:xs) = localToString x ++ "\n" ++ toWrite xs
+toWrite :: Local -> String
+toWrite local = localToString local ++ "\n"
 
 --------- Visualizar Local ------------
 getLocaisPuros :: [Local]
@@ -97,7 +100,7 @@ getLocaisPuros = unsafePerformIO getLocaisFromList :: [Local]
 
 getLocaisFromList :: IO [Local]
 getLocaisFromList = do
-    locais <- openFile "../Arquivos/Locais.csv" ReadMode
+    locais <- openFile "Locais.csv" ReadMode
     listaDeLocais <- lines <$> hGetContents locais
     hClose locais
     return $ convertToList listaDeLocais
@@ -111,28 +114,28 @@ convertRecursosToList :: [String] -> [[String]]
 convertRecursosToList = map (`split` ',')
 
 convertToLocal :: [String] -> Local
-convertToLocal local = Local idLocal nomeLocal recursosLocal capacidadeLocal
+convertToLocal local = Local nomeLocal recursosLocal capacidadeLocal
     where
-        idLocal = read (head local) :: Int
+        --idLocal = read (head local) :: Int
         nomeLocal = local !! 1
-        recursosLocal = fromIO (getRecursosList idLocal)
+        recursosLocal = fromIO (getRecursosList nomeLocal)
         capacidadeLocal = read (local !! 3) :: Int
 
-getRecursosList :: Int -> IO [String]
-getRecursosList id = do
-    recursos <- openFile "../Arquivos/RecursosLocal.csv" ReadMode
+getRecursosList :: String -> IO [String]
+getRecursosList nome = do
+    recursos <- openFile "RecursosLocal.csv" ReadMode
     listaDeRecursos <- lines <$> hGetContents recursos
     let recursosTotais = convertRecursosToList listaDeRecursos
     hClose recursos
-    return $ filtraRecurso id recursosTotais
+    return $ filtraRecurso nome recursosTotais
 
-filtraRecurso :: Int -> [[String]] -> [String]
+filtraRecurso :: String -> [[String]] -> [String]
 filtraRecurso _ [] = []
-filtraRecurso id (recurso:recursos)
-    | id == idLocal = tail recurso
-    | otherwise = filtraRecurso id recursos
+filtraRecurso nome (recurso:recursos)
+    | nome == nomeLocal = tail recurso
+    | otherwise = filtraRecurso nome recursos
     where
-        idLocal = read (head recurso) :: Int
+        nomeLocal = read (head recurso) :: String
 
 ---- Retirar impurezas do IO ----
 fromIO :: IO[String] -> [String]
